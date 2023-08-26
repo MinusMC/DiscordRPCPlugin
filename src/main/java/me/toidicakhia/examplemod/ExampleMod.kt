372946eadb344710959c86aae75024273ec4cf3a
toithichcakhia <toidicakhia1234@gmail.com>
2023-08-26 12:59:10 +0700

    update

--- src/main/java/me/toidicakhia/examplemod/ExampleMod.kt
+++
@@ -1,9 +0,0 @@
-package me.toidicakhia.examplemod
-
-import net.ccbluex.liquidbounce.plugin.Plugin
-
-class ExampleMod: Plugin("ExampleMod", "dev") {
-	override fun init() {
-		println("This is an example mod!!!")
-	}
-}
---
+++ src/main/java/net/minusmc/discordrpcplugin/DiscordRPCPlugin.kt
@@ -0,0 +1,52 @@
+package me.toidicakhia.examplemod
+
+import net.ccbluex.liquidbounce.plugin.Plugin
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.ClientShutdownEvent
+import net.minusmc.discordrpcplugin.DiscordRPCPlugin
+
+class DiscordRPCPlugin: Plugin("DiscordRPCPlugin", "dev") {
+
+	lateinit var clientRichPresence: ClientRichPresence
+
+	override fun init() {
+		clientRichPresence = ClientRichPresence()
+		if (clientRichPresence.showRichPresenceValue) {
+            thread {
+                try {
+                    clientRichPresence.setup()
+                } catch (throwable: Throwable) {
+                    ClientUtils.getLogger().error("Failed to setup Discord RPC.", throwable)
+                }
+            }
+        }
+	}
+
+	@EventTarget
+	fun onShutDown(event: ClientShutdownEvent) {
+		clientRichPresence.shutdown()
+	}
+
+	// } else if (extendedModMode) {
+                    //         val rpc = LiquidBounce.clientRichPresence
+                    //         rpc.showRichPresenceValue = when (val state = !rpc.showRichPresenceValue) {
+                    //             false -> {
+                    //                 rpc.shutdown()
+                    //                 false
+                    //             }
+                    //             true -> {
+                    //                 var value = true
+                    //                 thread {
+                    //                     value = try {
+                    //                         rpc.setup()
+                    //                         true
+                    //                     } catch (throwable: Throwable) {
+                    //                         ClientUtils.getLogger().error("Failed to setup Discord RPC.", throwable)
+                    //                         false
+                    //                     }
+                    //                 }
+                    //                 value
+                    //             }
+                    //         }
+                    //     } 
+}
